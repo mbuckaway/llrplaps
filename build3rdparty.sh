5e0ef285d69d0efb09dc1ed60b76b30d4ef083dc
Mark Buckaway <mark@buckaway.ca>
2017-09-18 20:16:35 -0400

    Added build script for thirdparty on mac/linuux

---
+++ build3rdparty.sh
@@ -0,0 +1,13 @@
+@echo off
+echo Building Thirdparty
+mkdir thirdparty
+cd thirdparty
+echo Getting LLRPLTK....
+git clone https://github.com/mbuckaway/LLRPToolkit LLRPToolkit
+cd LLRPToolkit
+mkdir build
+cd build
+cmake -G "Makefiles"
+
+
+
